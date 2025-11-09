dc3d92db886ac8d6f077ab86f9fa56455092021c
samir <sa.samir213002019@gmail.com>
2025-11-09 11:58:23 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+
+//this 1st code
+
+console.log("hello word");
+
+//this is  variable and data type
+
+let name="samir"
+
+
+let age="23"
+
+let isstudent=true;
+
+
+
+console.log(name);
+console.log(age);
+console.log(isstudent);
+
+
+// here is start 2 number arithimtic
+
+let a=10;
+let b=5;
+
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+
+console.log*(a/b);
+
+
+// if else odd even
+
+
+
+
+
+
+let num=7;
+if(num % 2==0){
+
+
+
+
+console.log( "this number is even");
+}
+
+
+else{
+console.log("this is odd number")
+
+
+
+}
+
+
+
+
+
+
+
+
+//loop
+
+
+//for loop
+
+for(let i=0; i<10; i++){
+
+    console.log(i)
+}
+
+
+//while loop
+
+
+
+let j=0;
+while(j<5){
+    console.log(j);
+    j++
+}
+
+
+//function
+
+
+function great(){
+    console.log("hello vai")}
+
+great();
+
+//function
+
+function greet(name){
+
+return "hello" + name;
+
+
+}
+
+console.log(greet("samir"));
+
+
+
+//type of variable
+
+
+let x=4;
+
+console.log(typeof(x));
+
+
+let number = 10;
+let number2 = "10";
+console.log(number == number2);
